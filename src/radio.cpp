c9729d9626c1d4669d9ed61a86ca416b2103346b
Nathaniel Bock <bock.nathaniel@gmail.com>
2024-02-23 15:34:55 -0500

    Move from xbee to generic serial radio

--- src/radio.cpp
+++ src/radio.cpp
@@ -6,17 +6,17 @@
 
 void radio_sync()
 {
-	XBEE_SERIAL.write("\xdb\x69\xc0\x78", 4);
+	RADIO_SERIAL.write("\xdb\x69\xc0\x78", 4);
 }
 
 void radio_send(const Packet &pkt)
 {
 	const byte *pkt_buf = reinterpret_cast<const byte *>(&pkt);
-	XBEE_SERIAL.write(pkt_buf, pkt.getLen());
+	RADIO_SERIAL.write(pkt_buf, pkt.getLen());
 }
 
 void radio_setup()
 {
-	XBEE_SERIAL.begin(115200);
+	RADIO_SERIAL.begin(115200);
 	scheduler_add(TaskId::PacketSync, Task(radio_sync, 20 * KALMAN_PERIOD * 1000L, 20));
 }
